0dcce6e45b854e314dd1bf00c4b427836fdd0773
arakaru-botcreate <75591809+arakaru-botcreate@users.noreply.github.com>
2020-12-07 11:17:43 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,49 +1,32 @@
 import discord
-
+import random
 client = discord.Client()
-
-
-@client.event
-async def on_raw_reaction_add(payload):
-
-    if payload.message_id == 785326919545520148:
-
-        print(payload.emoji.name)
-        guild_id = payload.guild_id
-        guild = discord.utils.find(lambda g: g.id == guild_id, client.guilds)
-
-        role = discord.utils.find(lambda r: r.name == payload.emoji.name, guild.roles)
-
-        if role is not None:
-            print(role.name + " was found!")
-            print(role.id)
-            member = discord.utils.find(lambda m: m.id == payload.user_id, guild.members)
-            await member.add_roles(role)
-            print("done")
-
-
-@client.event
-async def on_raw_reaction_remove(payload):
-    if payload.message_id == 785326919545520148:
-        print(payload.emoji.name)
-
-        guild_id = payload.guild_id
-        guild = discord.utils.find(lambda g: g.id == guild_id, client.guilds)
-        role = discord.utils.find(lambda r: r.name == payload.emoji.name, guild.roles)
-
-        if role is not None:
-            member = discord.utils.find(lambda m: m.id == payload.user_id, guild.members)
-            await member.remove_roles(role)
-            print("done")
-
+token = os.environ['DISCORD_BOT_TOKEN']
+random_contents = [
+    "にゃーん",
+    "わん！",
+    "コケッコッコー",
+]
 
 @client.event
 async def on_ready():
-    print("Botは正常に起動しました！")
-    print(client.user.name)  # ボットの名前
-    print(client.user.id)  # ボットのID
-    print(discord.__version__)  # discord.pyのバージョン
-    print('------')
-    await client.change_presence(activity=discord.Game(name="役職を管理！"))
+    print("on_ready")
+    print(discord.__version__)
 
-client.run("TOKEN")
+
+@client.event
+async def on_message(message):
+    # 送信者がbotである場合は弾く
+    if message.author.bot:
+        return 
+    # メッセージの本文が 鳴いて だった場合
+    if message.content == "鳴いて":
+        # 送信するメッセージをランダムで決める
+        content = random.choice(random_contents)
+        # メッセージが送られてきたチャンネルに送る
+        await message.channel.send(content)
+    elif message.content == "おはよう":
+        await message.channel.send("おはよう！！")
+
+
+client.run("あなたのbotのトークン")
