21cf3803c6cb7956b3d701e1c779c3b1293ebf09
arakaru-botcreate <75591809+arakaru-botcreate@users.noreply.github.com>
2020-12-07 11:49:42 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,21 +2,49 @@ import discord
 import os
 
 token = os.environ['DISCORD_BOT_TOKEN']
-
-import discord
-
 client = discord.Client()
 
 @client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+async def on_raw_reaction_add(payload):
+
+    if payload.message_id == ID:
+
+        print(payload.emoji.name)
+        guild_id = payload.guild_id
+        guild = discord.utils.find(lambda g: g.id == guild_id, client.guilds)
+
+        role = discord.utils.find(lambda r: r.name == payload.emoji.name, guild.roles)
+
+        if role is not None:
+            print(role.name + " was found!")
+            print(role.id)
+            member = discord.utils.find(lambda m: m.id == payload.user_id, guild.members)
+            await member.add_roles(role)
+            print("done")
+
 
 @client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+async def on_raw_reaction_remove(payload):
+    if payload.message_id == ID:
+        print(payload.emoji.name)
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+        guild_id = payload.guild_id
+        guild = discord.utils.find(lambda g: g.id == guild_id, client.guilds)
+        role = discord.utils.find(lambda r: r.name == payload.emoji.name, guild.roles)
+
+        if role is not None:
+            member = discord.utils.find(lambda m: m.id == payload.user_id, guild.members)
+            await member.remove_roles(role)
+            print("done")
+
+
+@client.event
+async def on_ready():
+    print("Botは正常に起動しました！")
+    print(client.user.name)  # ボットの名前
+    print(client.user.id)  # ボットのID
+    print(discord.__version__)  # discord.pyのバージョン
+    print('------')
+    await client.change_presence(activity=discord.Game(name="役職を管理！"))
 
 client.run(token)
